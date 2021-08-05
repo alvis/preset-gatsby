5a46fdd2ed523d944cf6aa65ccc0922dded7ab03
Alvis HT Tang <alvis@hilbert.space>
2021-08-05 20:40:05 +0100

    init: provide a presetter-preset extension

---
+++ source/index.ts
@@ -0,0 +1,27 @@
+/*
+ *                            *** MIT LICENSE ***
+ * -------------------------------------------------------------------------
+ * This code may be modified and distributed under the MIT license.
+ * See the LICENSE file for details.
+ * -------------------------------------------------------------------------
+ *
+ * @summary   Collection of preset assets for a Gatsby project
+ *
+ * @author    Alvis HT Tang <alvis@hilbert.space>
+ * @license   MIT
+ * @copyright Copyright (c) 2021 - All Rights Reserved.
+ * -------------------------------------------------------------------------
+ */
+
+/* istanbul ignore file */
+
+import type { PresetAsset } from 'presetter';
+
+/**
+ * get the list of templates provided by this preset
+ * @returns list of preset templates
+ */
+export default async function (): Promise<PresetAsset> {
+  return {
+  };
+}
